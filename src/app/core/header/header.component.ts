44eb920ee33b3aca203ebb41702859484a0efb93
Ematius <eksaber87@gmail.com>
2025-04-04 06:37:13 +0200

    menu progress 40%

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -8,35 +8,35 @@ import { ViewportScroller } from '@angular/common';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent implements OnInit {
-  activeSection = 'home';
-  isMobile = false;
+export class HeaderComponent {
+  // activeSection = 'home';
+  // isMobile = false;
 
-  constructor(private viewportScroller: ViewportScroller) {}
-  toggleMenu() {
-    this.isMobile = !this.isMobile;
-  }
+  // constructor(private viewportScroller: ViewportScroller) {}
+  // toggleMenu() {
+  //   this.isMobile = !this.isMobile;
+  // }
 
-  scrollToSection(sectionId: string) {
-    this.viewportScroller.scrollToAnchor(sectionId);
-  }
-  ngOnInit() {
-    const sections = document.querySelectorAll('section[id]');
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            this.activeSection = entry.target.id;
-          }
-        });
-      },
-      {
-        rootMargin: '-50% 0px -50% 0px',
-      }
-    );
+  // scrollToSection(sectionId: string) {
+  //   this.viewportScroller.scrollToAnchor(sectionId);
+  // }
+  // ngOnInit() {
+  //   const sections = document.querySelectorAll('section[id]');
+  //   const observer = new IntersectionObserver(
+  //     (entries) => {
+  //       entries.forEach((entry) => {
+  //         if (entry.isIntersecting) {
+  //           this.activeSection = entry.target.id;
+  //         }
+  //       });
+  //     },
+  //     {
+  //       rootMargin: '-50% 0px -50% 0px',
+  //     }
+  //   );
 
-    sections.forEach((section) => observer.observe(section));
-  }
+  //   sections.forEach((section) => observer.observe(section));
+  // }
 
 
 }
