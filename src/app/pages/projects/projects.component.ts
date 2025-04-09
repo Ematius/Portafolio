48a0eb93b666b04654a3d29f1674ceb5227016bf
Ematius <eksaber87@gmail.com>
2025-04-09 17:31:34 +0200

    initial NewPorfolio

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [ CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements AfterViewInit{
+
+  visible = false;
+
+  @ViewChild('titleRef') titleRef!: ElementRef;
+
+  ngAfterViewInit(): void {
+   const observer = new IntersectionObserver(
+     ([entry]) => {
+        if (entry.isIntersecting) {
+          this.visible = true;
+          observer.disconnect(); // Solo una vez
+        }
+      },
+      { threshold: 0.5 } // Cuando entra el 50%
+    );
+
+    observer.observe(this.titleRef.nativeElement);
+  }
+
+
+
+
 
 }
