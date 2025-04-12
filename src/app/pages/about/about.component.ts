b526b2435d0b837c03a70f7624168e0931e9ee86
Ematius <eksaber87@gmail.com>
2025-04-12 13:15:41 +0200

    Add animation gsap

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-about',
+  standalone: true,
   imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent {
+export class AboutComponent implements AfterViewInit {
+  @ViewChild('articleRef', { static: true }) articleRef!: ElementRef;
+  @ViewChild('divTextRef', { static: true }) divTextRef!: ElementRef;
+  @ViewChild('cardRef', { static: true }) cardRef!: ElementRef;
 
+  ngAfterViewInit(): void {
+    ScrollTrigger.matchMedia({
+
+      '(min-width: 1400px)': () => {
+        const tl = gsap.timeline({
+          scrollTrigger: {
+            trigger: this.articleRef.nativeElement,
+            start: 'top top',
+            end: '+=2000',
+            scrub: true,
+            pin: true,
+            markers: false,
+          },
+        });
+
+
+        tl.from(this.cardRef.nativeElement, {
+          x: 200,
+          opacity: 0,
+          duration: 1,
+          ease: 'power2.out',
+        });
+
+       
+        tl.from(this.divTextRef.nativeElement, {
+          x: -200,
+          opacity: 0,
+          duration: 1,
+          ease: 'power2.out',
+        });
+      },
+
+      // Otras resoluciones: nada, o animaciones alternativas si querés
+      all: () => {
+        // Esto se ejecuta siempre. Útil si querés limpiar cosas, resetear estilos, etc.
+      },
+    });
+  }
 }
