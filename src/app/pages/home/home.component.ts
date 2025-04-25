71a6bb4ee1c5258feb9ea205a14e185a4bf375e5
Ematius <eksaber87@gmail.com>
2025-04-25 03:25:41 +0200

    configure line endings and binary handling with .gitattributes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -20,12 +20,5 @@ export class HomeComponent implements OnInit {
     const tl = gsap.timeline({ delay: 1 });
     tl.fromTo('h1', { text: '' }, { text: 'Bienvenido', duration: 1 });
 
-    const tl2 = gsap.timeline({
-      delay: 1,
-      repeat: 1,
-      repeatDelay: 0.1,
-      yoyo: true,
-    });
-    tl2.fromTo('h3', { text: '' }, { text: 'a mi portafolio', duration: 1 }, '<');
   }
 }
