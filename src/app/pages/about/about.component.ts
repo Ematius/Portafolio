ff614dccc77f73bd96f066287840b119215ef7c5
Ematius <eksaber87@gmail.com>
2025-04-25 11:45:57 +0200

    change knowledge component to button

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -12,6 +12,52 @@ gsap.registerPlugin(ScrollTrigger);
   styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements AfterViewInit {
+  showSkills = false;
+
+  toggleSkills() {
+    if (this.showSkills) {
+      gsap.to('.skill-item', {
+        opacity: 0,
+        scale: 0.5,
+        y: -20,
+        stagger: 0.05,
+        duration: 0.5,
+        ease: 'power2.in',
+        onComplete: () => {
+          this.showSkills = false;
+        },
+      });
+    } else {
+      this.showSkills = true;
+      setTimeout(() => {
+        gsap.from('.skill-item', {
+          opacity: 0,
+          scale: 0.5,
+          y: -20,
+          stagger: 0.05,
+          duration: 0.6,
+          ease: 'power2.out',
+        });
+      });
+    }
+  }
+  icons = [
+    { name: 'HTML', file: 'HTML.svg' },
+    { name: 'CSS', file: 'CSS.svg' },
+    { name: 'JS', file: 'javascript.svg' },
+    { name: 'TS', file: 'typescript.svg' },
+    { name: 'Angular', file: 'angular.svg' },
+    { name: 'Node', file: 'Node.svg' },
+    { name: 'Express', file: 'express.svg' },
+    { name: 'MySQL', file: 'mysql.svg' },
+    { name: 'MongoDB', file: 'mongodb.svg' },
+    { name: 'Vitest', file: 'vitest.svg' },
+    { name: 'Jest', file: 'Jest.svg' },
+    { name: 'Prettier', file: 'prettier.svg' },
+    { name: 'ESLint', file: 'ESLint.svg' },
+    { name: 'Postman', file: 'postman.svg' },
+  ];
+
   @ViewChild('articleRef', { static: true }) articleRef!: ElementRef;
   @ViewChild('divTextRef', { static: true }) divTextRef!: ElementRef;
   @ViewChild('cardRef', { static: true }) cardRef!: ElementRef;
