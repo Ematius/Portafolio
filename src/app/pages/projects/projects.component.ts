5bd7bdb87db0335519e06c42161d9001c73725fa
Ematius <eksaber87@gmail.com>
2025-04-26 07:24:45 +0200

    component.about finished

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+import { Component,  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
@@ -11,33 +11,7 @@ gsap.registerPlugin(ScrollTrigger);
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
-export class ProjectsComponent implements OnInit {
-  @ViewChild('titleRef', { static: false }) titleRef!: ElementRef;
-  @ViewChild('divRef', { static: false }) divRef!: ElementRef;
+export class ProjectsComponent  {
 
-  ngOnInit(){
-    gsap.from(this.titleRef.nativeElement, {
-      x: -100,
-      opacity: 0,
-      duration: 1,
-      ease: 'sine.inOut',
-      scrollTrigger: {
-        trigger: this.titleRef.nativeElement,
-        start: 'top 80%',
-        toggleActions: 'restart none restart none',
-      },
-    });
-    gsap.from(this.divRef.nativeElement, {
-      x: 100,
-      opacity: 0,
-      duration: 1,
-      ease: 'sine.inOut',
-      scrollTrigger: {
-        trigger: this.divRef.nativeElement,
-        start: 'top 80%',
-        toggleActions: 'play none none reverse',
-      },
-    });
-  }
 }
 
