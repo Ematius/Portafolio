f42613fc894bf65c21d5e3c9e94952a21d69cd5c
Ematius <eksaber87@gmail.com>
2025-07-14 13:32:47 +0200

    update CV

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,10 @@ import { ContactComponent } from './pages/contact/contact.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 
 export const routes: Routes = [
-  { path: '**', redirectTo: 'home' },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'Home', component: HomeComponent },
   { path: 'About', component: AboutComponent },
   { path: 'Projects', component: ProjectsComponent },
   { path: 'Contact', component: ContactComponent },
+  { path: '**', redirectTo: 'home' },
 ];
